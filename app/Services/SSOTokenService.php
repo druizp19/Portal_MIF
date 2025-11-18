067f2c423907756ebdec7dcedb3ed2ec59103fb2
Medifarma <druizp@medifarma.com.pe>
2025-11-18 09:18:53 -0500

    cambios 18/11

--- app/Services/SSOTokenService.php
+++ app/Services/SSOTokenService.php
@@ -12,7 +12,14 @@ class SSOTokenService
 
     public function __construct()
     {
-        $this->secretKey = config('app.key');
+        $key = config('app.key');
+        
+        // Laravel siempre incluye el prefijo base64: en APP_KEY, decodificarlo
+        if (str_starts_with($key, 'base64:')) {
+            $this->secretKey = base64_decode(substr($key, 7));
+        } else {
+            $this->secretKey = $key;
+        }
     }
 
     public function generateToken(array $userData): string
